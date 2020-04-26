6cf9bac7ad387b646bb4a6db1c55bcaf20dc99e7
dbenitez83@live.com <dbenitez@uoc.edu>
2020-04-26 11:26:08 +0200

    Añadido menú

---
+++ app/screens/TopRestaurants.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function TopRestaurantsScreen(){
+    return(
+        <View>
+            <Text>Estamos en el ranking de restaurantes.</Text>
+        </View>
+    );
+}
